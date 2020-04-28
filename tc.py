3726f1d1ed1cc073dc9b6f541d320ef2e39590bf
Nathan Yin <yinzeqi11@gmail.com>
2020-04-28 19:16:30 +1200

    Correction

--- tc.py
+++ tc.py
@@ -1,5 +1,5 @@
 celsius = input('Please input the figure of Celsius temperature: ')
 celsius = float(celsius)
-fahrenhite = float(celsius * 9/5 + 32)
+fahrenheit = float(celsius * 9/5 + 32)
 
-print('Fahrenhite temperature is', fahrenhite)
+print('Fahrenheit temperature is', fahrenheit)
