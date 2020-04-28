36dfc46bf948fe17816a9cea86f824798a3366d1
Nathan Yin <yinzeqi11@gmail.com>
2020-04-28 14:43:22 +1200

    Add New Flie

---
+++ tc.py
@@ -0,0 +1,5 @@
+celsius = input('Please input the figure of Celsius temperature: ')
+celsius = float(celsius)
+fahrenhite = float(celsius * 9/5 + 32)
+
+print('Fahrenhite temperature is', fahrenhite)
